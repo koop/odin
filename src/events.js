a986406c68c9e68c89d9034ecc7378168fbfb72e
Daryl Koopersmith <daryl.koopersmith@gmail.com>
2012-08-13 20:03:11 -0700

    Initial import.

---
+++ src/events.js
@@ -0,0 +1,292 @@
+/*
+ * events
+ * https://github.com/koop/events
+ *
+ * Copyright (c) 2012 Daryl Koopersmith
+ * Licensed under the GPL license, version 2 or greater.
+ */
+
+var Event, Events;
+
+(function( _ ) {
+	var slice = Array.prototype.slice;
+
+	/**
+	 * EVENT
+	 */
+
+	Event = function( options ) {
+		_.extend( this, options );
+		this.callbacks = [];
+	};
+
+
+	_.extend( Event.prototype, {
+		/**
+		 * Adds a callback to the event.
+		 *
+		 * @param {object}   options    The callback's options.
+		 *        {function} fn         The callback function.
+		 *        {object}   context    Optional. The context in which to call the function (the 'this' keyword).
+		 *        {object}   namespace  Optional. An object with namespace keys mapped to 'true'.
+		 *        {integer}  priority   Optional. Default 10. The priority of the callback.
+		 */
+		add: function( options ) {
+			var defaults, low, mid, high;
+
+			if ( ! options.fn )
+				return;
+
+			options = _.defaults( options, {
+				priority: 10,
+				namespace: {}
+			});
+
+			// Cache the callback we'll actually use.
+			options.bound = _.bind( options.fn, options.context );
+
+			// Handle priorities using binary search.
+			// Thank you, underscore.js! Inspired by _.sortedIndex.
+			low = 0;
+			high = this.callbacks.length;
+			while ( low < high ) {
+				mid = ( low + high ) >> 1; // Bit shift to divide by two.
+				if ( this.callbacks[ mid ].priority <= options.priority )
+					low = mid + 1;
+				else
+					high = mid;
+			}
+
+			this.callbacks.splice( low, 0, options );
+			return this;
+		},
+
+		once: function( options ) {
+			if ( ! options.fn )
+				return;
+
+			var callback = options.fn,
+				self     = this;
+
+			options.fn = function() {
+				callback.apply( this, arguments );
+				self.remove( options.fn );
+			};
+
+			return this.add( options );
+		},
+
+		_filter: function( options, reject ) {
+			options = options || {};
+			return _[ reject ? 'reject' : 'filter' ]( this.callbacks, function( callback ) {
+				return _.all( options, function( value, key ) {
+					if ( 'namespace' === key ) {
+						return _.all( value, function( v, ns ) {
+							return callback.namespace[ ns ];
+						});
+					} else {
+						return value === callback[ key ];
+					}
+				});
+			}, this );
+		},
+
+		_reject: function( options ) {
+			return this._filter( options, true );
+		},
+
+		remove: function( options ) {
+			this.callbacks = this._reject( options );
+			return this;
+		},
+
+		// trigger( [options], [iterator], [args] )
+		trigger: function( options, iterator, args ) {
+			// trigger( args )
+			if ( _.isArray( options ) ) {
+				args     = options;
+				iterator = null;
+				options  = null;
+
+			// trigger( iterator, [args] )
+			} else if ( _.isFunction( options ) || _.isString( options ) ) {
+				args     = iterator;
+				iterator = options;
+				options  = null;
+
+			// trigger( options, args )
+			} else if ( _.isArray( iterator ) ) {
+				args     = iterator;
+				iterator = null;
+			}
+
+			// Iterator can either be a function, or a string that corresponds
+			// to a registered iterator.
+			if ( _.isString( iterator ) )
+				iterator = this.iterators[ iterator ];
+			iterator = iterator || this.iterators.each;
+
+			// Ensure args is an array
+			args = args || [];
+
+			// Filters the callbacks and prevents race conditions
+			// by not directly referring to this.callbacks.
+			// Then, pluck all of the bound callbacks so the iterator isn't
+			// accessing the direct object.
+			var callbacks = _.pluck( this._filter( options ), 'bound' );
+
+			return iterator.call( this, callbacks, args );
+		},
+
+		iterators: {
+			each: function( callbacks, args ) {
+				_.each( callbacks, function( callback ) {
+					callback.apply( null, args );
+				});
+			},
+
+			reduce: function( callbacks, args ) {
+				return _.reduce( callbacks, function( memo, callback ) {
+					return callback.apply( null, [ memo ].concat( args ) );
+				}, args.shift() );
+			},
+
+			all: function( callbacks, args ) {
+				return _.all( callbacks, function( callback ) {
+					return !! callback.apply( null, args );
+				});
+			},
+
+			any: function( callbacks, args ) {
+				return _.any( callbacks, function( callback ) {
+					return !! callback.apply( null, args );
+				});
+			}
+		}
+	});
+
+	// Create aliases for 'action' and 'filter'
+	_.extend( Event.prototype.iterators, {
+		action: Event.prototype.iterators.each,
+		filter: Event.prototype.iterators.reduce
+	});
+
+
+	/**
+	 * EVENTS
+	 */
+
+	Events = function() {
+		this.events = {};
+	};
+
+	_.extend( Events.prototype, {
+		/**
+		 * Processes event strings and automatically creates events.
+		 *
+		 * @param  {string}   events      A string that represents a series of events and namespaces.
+		 *                                Events are separated by spaces. Namespaces are separated by dots.
+		 * @param  {Function} iterator    The callback to iterate over the events with.
+		 *                                Signature: iterator( event, options )
+		 * @param  {object}   options     Options for the process method.
+		 *         {boolean}  idRequired  Whether an id is required for each event.
+		 */
+		process: function( events, iterator, options ) {
+			options = _.defaults( options || {}, {
+				idRequired: true
+			});
+
+			var result = _( events.split(' ') ).chain().map( function( value, key ) {
+				var namespaces = value.split('.'),
+					params = {
+						id: namespaces.shift(),
+						namespace: {}
+					};
+
+				_.each( namespaces, function( key ) {
+					params.namespace[ key ] = true;
+				});
+
+				// If we have an ID, everything is normal.
+				if ( params.id )
+					return params;
+
+				// If no ID is supplied, we either ignore this event
+				// or apply the params to every event.
+				if ( options.idRequired || ! namespaces.length ) {
+					return false;
+				} else {
+					return _.map( this.events, function( event ) {
+						return _.extend( {}, params, { id: event.id } );
+					});
+				}
+			}, this ).flatten().compact().map( function( params ) {
+				if ( ! this.events[ params.id ] )
+					this.events[ params.id ] = new Event({ id: params.id });
+				return iterator.call( this, this.events[ params.id ], { namespace: params.namespace } );
+			}, this ).value();
+
+			// If only one event was provided, return the result.
+			// Otherwise, return an array of results.
+			return ( 1 === result.length ) ? result[0] : result;
+		},
+
+		// _on( method, events, callback, [context], [priority] )
+		_on: function( method, events, fn, context, priority ) {
+			// Handle when context is not set, but priority is.
+			if ( _.isNumber( context ) ) {
+				priority = context;
+				context = null;
+			}
+
+			var callback = {
+				fn: fn,
+				context: context,
+				priority: priority
+			};
+
+			this.process( events, function( event, options ) {
+				event[ method ]( _.extend( options, callback ) );
+			});
+		},
+
+		// on( events, callback, [context], [priority] )
+		on: function( events, fn, context, priority ) {
+			this._on( 'add', events, fn, context, priority );
+		},
+
+		// once( events, callback, [context], [priority] )
+		once: function( events, fn, context, priority ) {
+			this._on( 'once', events, fn, context, priority );
+		},
+
+		off: function( events, fn ) {
+			this.process( events, function( event, options ) {
+				if ( fn )
+					options.fn = fn;
+				event.remove( options );
+			}, { idRequired: false });
+		},
+
+		// trigger( events, [iterator], [args] )
+		trigger: function( events, iterator, args ) {
+			return this.process( events, function( event, options ) {
+				return event.trigger( options, iterator, args );
+			});
+		}
+	});
+
+	// Create shortcuts for default iterators
+	_.each( Event.prototype.iterators, function( iterator, method ) {
+
+		// Don't overwrite existing methods
+		if ( Events.prototype[ method ] )
+			return;
+
+		// iteratorName( events, args* )
+		Events.prototype[ method ] = function( events ) {
+			return this.trigger( events, method, _.rest( arguments ) );
+		};
+	});
+
+}( _ ) );
---
+++ test/events_test.js
@@ -0,0 +1,270 @@
+/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+(function($) {
+
+	/*
+		======== A Handy Little QUnit Reference ========
+		http://docs.jquery.com/QUnit
+
+		Test methods:
+			expect(numAssertions)
+			stop(increment)
+			start(decrement)
+		Test assertions:
+			ok(value, [message])
+			equal(actual, expected, [message])
+			notEqual(actual, expected, [message])
+			deepEqual(actual, expected, [message])
+			notDeepEqual(actual, expected, [message])
+			strictEqual(actual, expected, [message])
+			notStrictEqual(actual, expected, [message])
+			raises(block, [expected], [message])
+	*/
+
+	module( 'Events', {
+		setup: function() {
+			this.events = new Events();
+		}
+	});
+
+	test( 'add / trigger / remove an event', 3, function() {
+		this.events.on( 'a', function() {
+			ok( true, 'triggered event "a"' );
+		});
+		equal( this.events.events.a.callbacks.length, 1, 'added event "a"' );
+		this.events.trigger( 'a' );
+		this.events.off( 'a' );
+		equal( this.events.events.a.callbacks.length, 0, 'removed event "a"' );
+	});
+
+	test( 'add / trigger / remove multiple events', 6, function() {
+		this.events.on( 'a b', function() {
+			ok( true, 'triggered event "a" or "b"' );
+		});
+		equal( this.events.events.a.callbacks.length, 1, 'added event "a"' );
+		equal( this.events.events.b.callbacks.length, 1, 'added event "b"' );
+
+		this.events.trigger( 'a b' );
+
+		this.events.off( 'a b' );
+		equal( this.events.events.a.callbacks.length, 0, 'removed event "a"' );
+		equal( this.events.events.b.callbacks.length, 0, 'removed event "a"' );
+	});
+
+	test( 'once', 3, function() {
+		this.events.once( 'a', function() {
+			ok( true, 'triggered event "a"' );
+		});
+		equal( this.events.events.a.callbacks.length, 1, 'added event "a"' );
+		this.events.trigger( 'a' ); // Should assert 'triggered event "a"'
+		equal( this.events.events.a.callbacks.length, 0, 'automatically removed event "a"' );
+		this.events.trigger( 'a' ); // Nothing should happen.
+	});
+
+	test( 'once, multiple', 6, function() {
+		this.events.once( 'a b', function() {
+			ok( true, 'triggered event "a" or "b"' );
+		});
+		equal( this.events.events.a.callbacks.length, 1, 'added event "a"' );
+		this.events.trigger( 'a' ); // Should assert 'triggered event "a" or "b"'
+		equal( this.events.events.a.callbacks.length, 0, 'automatically removed event "a"' );
+		this.events.trigger( 'a' ); // Nothing should happen.
+
+		equal( this.events.events.b.callbacks.length, 1, 'event "b" still bound' );
+		this.events.trigger( 'b' ); // Should assert 'triggered event "a" or "b"'
+		equal( this.events.events.b.callbacks.length, 0, 'automatically removed event "b"' );
+		this.events.trigger( 'b' ); // Nothing should happen.
+	});
+
+	module( 'Namespaces', {
+		setup: function() {
+			this.events = new Events();
+
+			var triggered = [];
+
+			this.events.record = function( events ) {
+				_.each( events, function( event ) {
+					this.on( event, function() { triggered.push( event ); });
+				}, this );
+			};
+
+			// Order-insensitive
+			this.events.recorded = function( events ) {
+				events = _.uniq( events ).sort();
+				deepEqual( _.uniq( triggered ).sort(), events, 'recorded: ' + events.join(', ') );
+			};
+
+			this.events.record([ 'a', 'a.x', 'a.y', 'a.x.y', 'a.y.x', 'b', 'b.x', 'b.y', 'b.x.y', 'b.y.x' ]);
+		}
+	});
+
+	test( 'trigger event "a"', 1, function() {
+		this.events.trigger( 'a' );
+		this.events.recorded(['a','a.x','a.y','a.x.y','a.y.x']);
+	});
+
+	test( 'trigger event "a.x"', 1, function() {
+		this.events.trigger( 'a.x' );
+		this.events.recorded(['a.x','a.x.y','a.y.x']);
+	});
+
+	test( 'trigger event "a.x.y"', 1, function() {
+		this.events.trigger( 'a.x.y' );
+		this.events.recorded(['a.x.y','a.y.x']);
+	});
+
+	test( 'trigger event ".x"', 1, function() {
+		this.events.trigger( '.x' );
+		this.events.recorded([]);
+	});
+
+	test( 'remove "a"', 1, function() {
+		this.events.off( 'a' );
+		this.events.trigger( 'a' );
+		this.events.recorded([]);
+	});
+
+	test( 'remove "a.x"', 1, function() {
+		this.events.off( 'a.x' );
+		this.events.trigger( 'a' );
+		this.events.recorded(['a','a.y']);
+	});
+
+	test( 'remove "a.x.y"', 1, function() {
+		this.events.off( 'a.x.y' );
+		this.events.trigger( 'a' );
+		this.events.recorded(['a','a.x','a.y']);
+	});
+
+	test( 'remove "a.none"', 1, function() {
+		this.events.off( 'a.none' );
+		this.events.trigger( 'a' );
+		this.events.recorded(['a','a.x','a.y','a.x.y','a.y.x']);
+	});
+
+	test( 'remove "a.x.none"', 1, function() {
+		this.events.off( 'a.x.none' );
+		this.events.trigger( 'a' );
+		this.events.recorded(['a','a.x','a.y','a.x.y','a.y.x']);
+	});
+
+	test( 'remove ".x"', 1, function() {
+		this.events.off( '.x' );
+		this.events.trigger( 'a' );
+		this.events.trigger( 'b' );
+		this.events.recorded(['a','a.y','b','b.y']);
+	});
+
+	// Removing the empty string should leave everything intact.
+	test( 'remove ""', 1, function() {
+		this.events.off( '' );
+		this.events.trigger( 'a' );
+		this.events.trigger( 'b' );
+		this.events.recorded(['a','a.x','a.y','a.x.y','a.y.x','b','b.x','b.y','b.x.y','b.y.x']);
+	});
+
+	module( 'Priority – Implicit', {
+		setup: function() {
+			this.events = new Events();
+		}
+	});
+
+	test( 'trigger', function() {
+		var triggered = [];
+		_.each( _.range( 1, 6 ), function( index ) {
+			this.events.on( 'a', function() {
+				triggered.push( index );
+			});
+		}, this );
+
+		this.events.trigger( 'a' );
+		deepEqual( triggered, _.range( 1, 6 ), 'implicit priorities correct' );
+	});
+
+	module( 'Priority – Explicit', {
+		setup: function() {
+			this.events = new Events();
+
+			var triggered = [],
+				// Generate event based on priorty names.
+				priorityEvent = function( priority ) {
+					var event = [ 'count', priority ];
+
+					// Generate namespaces for odd, even, and multiples of three.
+					event.push( priority % 2 ? 'odd' : 'even' );
+					if ( 0 === (priority % 3) )
+						event.push('three');
+
+					return event.join('.');
+				};
+
+			// Add out of order
+			_.each([ 6, 2, 1, 4, 5, 3 ], function( priority ) {
+				var event = priorityEvent( priority );
+				this.events.on( event, function() { triggered.push( event ); }, priority );
+			}, this );
+
+			// Order-sensitive
+			this.events.recorded = function( priorities ) {
+				var events = _.map( priorities, priorityEvent );
+				deepEqual( triggered, events, 'recorded: ' + events.join(', ') );
+			};
+		}
+	});
+
+	test( 'trigger "count"', 1, function() {
+		this.events.trigger( 'count' );
+		this.events.recorded([ 1, 2, 3, 4, 5, 6 ]);
+	});
+
+	test( 'trigger "count.odd"', 1, function() {
+		this.events.trigger( 'count.odd' );
+		this.events.recorded([ 1, 3, 5 ]);
+	});
+
+	test( 'trigger "count.even"', 1, function() {
+		this.events.trigger( 'count.even' );
+		this.events.recorded([ 2, 4, 6 ]);
+	});
+
+	test( 'trigger "count.three"', 1, function() {
+		this.events.trigger( 'count.three' );
+		this.events.recorded([ 3, 6 ]);
+	});
+
+	module( 'Trigger – Reduce', {
+		setup: function() {
+			this.events = new Events();
+			this.events.reduce = function( events, args ) {
+				return this.trigger.call( this, events, 'reduce', args );
+			};
+		}
+	});
+
+	test( 'reduce various values', function() {
+		this.events.on( 'gamut', function( value ) {
+			strictEqual( value, 0 );
+			return true;
+		});
+
+		this.events.on( 'gamut', function( value ) {
+			strictEqual( value, true );
+			return { x: 20 };
+		});
+
+		this.events.on( 'gamut', function( value ) {
+			deepEqual( value, { x: 20 } );
+			value.y = 10;
+			return value;
+		});
+
+		this.events.on( 'gamut', function( value ) {
+			deepEqual( value, { x: 20, y: 10 } );
+			return value.x + value.y;
+		});
+
+		strictEqual( this.events.reduce( 'gamut', [ 0 ] ), 30, 'reduce successful' );
+	});
+
+}(jQuery));
